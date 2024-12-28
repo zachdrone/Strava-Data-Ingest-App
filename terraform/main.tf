b771dfe792a174c44ba8e8329c286cf181f433f8
Zach Drone <zvdrone@gmail.com>
2024-12-28 13:18:42 -0500

    fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "my-terraform-state-bucket"
+    bucket         = "zach-app-terraform-state-bucket"
     key            = "terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-locks"
