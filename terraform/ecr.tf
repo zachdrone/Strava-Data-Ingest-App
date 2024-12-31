0ff168d3ec74b99c1c8df6ac7cfbbbc7264b5275
Zach Drone <zvdrone@gmail.com>
2024-12-31 13:57:06 -0500

    add lifecycle policy to ecr

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -1,3 +1,27 @@
 resource "aws_ecr_repository" "my_lambda_repo" {
   name = "my-lambda-repo"
 }
+
+resource "aws_ecr_lifecycle_policy" "my_lambda_repo_lifecycle" {
+  repository = aws_ecr_repository.my_lambda_repo.name
+
+  policy = <<EOF
+  {
+      "rules": [
+          {
+              "rulePriority": 1,
+              "description": "Expire images older than 14 days",
+              "selection": {
+                  "tagStatus": "untagged",
+                  "countType": "sinceImagePushed",
+                  "countUnit": "days",
+                  "countNumber": 14
+              },
+              "action": {
+                  "type": "expire"
+              }
+          }
+      ]
+  }
+  EOF
+}
