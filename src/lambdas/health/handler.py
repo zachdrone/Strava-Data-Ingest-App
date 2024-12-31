df0a4ddfd2e95d2ba6e453751694d494426416cb
Zach Drone <zvdrone@gmail.com>
2024-12-31 11:35:21 -0500

    test updating helath

--- src/lambdas/health/handler.py
+++ src/lambdas/health/handler.py
@@ -4,7 +4,8 @@ from aws_lambda_powertools import Logger, Tracer
 logger = Logger(service="health-endpoint")
 tracer = Tracer(service="health-endpoint")
 
+
 @logger.inject_lambda_context(log_event=True)
 @tracer.capture_lambda_handler
 def lambda_handler(event, context):
-    return {"statusCode": 200, "body": "We are healthy!"}
+    return {"statusCode": 200, "body": "We are healthy updated!"}
