9c68f3919f4f2d668d571b28aab043eaec6fda0c
Zach Drone <zvdrone@gmail.com>
2024-12-31 14:00:55 -0500

    update lifecycle policy

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -6,22 +6,35 @@ resource "aws_ecr_lifecycle_policy" "my_lambda_repo_lifecycle" {
   repository = aws_ecr_repository.my_lambda_repo.name
 
   policy = <<EOF
-  {
-      "rules": [
-          {
-              "rulePriority": 1,
-              "description": "Expire images older than 14 days",
-              "selection": {
-                  "tagStatus": "untagged",
-                  "countType": "sinceImagePushed",
-                  "countUnit": "days",
-                  "countNumber": 14
-              },
-              "action": {
-                  "type": "expire"
-              }
-          }
-      ]
-  }
-  EOF
+{
+    "rules": [
+        {
+            "rulePriority": 1,
+            "description": "Expire images older than 14 days",
+            "selection": {
+                "tagStatus": "untagged",
+                "countType": "sinceImagePushed",
+                "countUnit": "days",
+                "countNumber": 14
+            },
+            "action": {
+                "type": "expire"
+            }
+        },
+        {
+            "rulePriority": 1,
+            "description": "Keep last 5 images",
+            "selection": {
+                "tagStatus": "tagged",
+                "tagPrefixList": ["v"],
+                "countType": "imageCountMoreThan",
+                "countNumber": 5
+            },
+            "action": {
+                "type": "expire"
+            }
+        }
+    ]
+}
+EOF
 }
