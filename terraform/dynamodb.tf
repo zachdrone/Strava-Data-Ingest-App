6825ab649c3a02b432c0b76327bed61a4f200668
Zach Drone <zvdrone@gmail.com>
2024-12-31 11:02:51 -0500

    try using sha for image tag

--- terraform/dynamodb.tf
+++ terraform/dynamodb.tf
@@ -8,6 +8,16 @@ resource "aws_dynamodb_table" "users" {
   }
 }
 
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
 resource "aws_dynamodb_table" "activities" {
   name         = "activities"
   billing_mode = "PAY_PER_REQUEST"
--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,3 +1,20 @@
+resource "aws_iam_role" "lambda_execution_role" {
+  name = "lambda-execution-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
 data "aws_iam_policy_document" "lambda_policy_doc" {
   statement {
     effect = "Allow"
