b6f5eb75ffcc58efc5adfc13763a0f0b39d644c9
Zach Drone <zachdrone@Zachs-MacBook-Pro.local>
2025-02-15 15:06:05 -0500

    add cache reop

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -2,8 +2,49 @@ resource "aws_ecr_repository" "my_lambda_repo" {
   name = "my-lambda-repo"
 }
 
+resource "aws_ecr_repository" "my_lambda_repo_cache" {
+  name = "my-lambda-repo-cache"
+}
+
 resource "aws_ecr_lifecycle_policy" "my_lambda_repo_lifecycle" {
   repository = aws_ecr_repository.my_lambda_repo.name
+
+  policy = <<EOF
+{
+    "rules": [
+        {
+            "rulePriority": 1,
+            "description": "Expire images older than 14 days",
+            "selection": {
+                "tagStatus": "untagged",
+                "countType": "sinceImagePushed",
+                "countUnit": "days",
+                "countNumber": 14
+            },
+            "action": {
+                "type": "expire"
+            }
+        },
+        {
+            "rulePriority": 2,
+            "description": "Keep last 5 images",
+            "selection": {
+                "tagStatus": "tagged",
+                "tagPrefixList": ["v"],
+                "countType": "imageCountMoreThan",
+                "countNumber": 5
+            },
+            "action": {
+                "type": "expire"
+            }
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_ecr_lifecycle_policy" "my_lambda_repo_lifecycle_cache" {
+  repository = aws_ecr_repository.my_lambda_repo_cache.name
 
   policy = <<EOF
 {
