0838ca18fbd9d0d0e22f923deae587cedad19771
Zach Drone <zachdrone@Zachs-MacBook-Pro.local>
2025-02-14 23:11:50 -0500

    json

--- terraform/sfn.tf
+++ terraform/sfn.tf
@@ -2,7 +2,7 @@ resource "aws_sfn_state_machine" "process_strava_data" {
   name     = "process-strava-data"
   role_arn = aws_iam_role.step_function_role.arn
 
-  definition = templatefile("${path.module}/templates/process_strava_data_sfn.tpl", {
+  definition = templatefile("${path.module}/templates/process_strava_data_sfn.json", {
     Region    = var.aws_region,
     AccountId = data.aws_caller_identity.current.account_id
   })
