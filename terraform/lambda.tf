965c691b0aa99451f00177be673213cde88442d6
Zach Drone <zachdrone@Zachs-MacBook-Pro.local>
2025-02-15 19:10:43 -0500

    docker image tag instead of latest

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,7 +1,7 @@
 resource "aws_lambda_function" "health_endpoint" {
   function_name = "health_endpoint"
   package_type  = "Image"
-  image_uri     = "${aws_ecr_repository.my_lambda_repo.repository_url}:latest"
+  image_uri     = "${aws_ecr_repository.my_lambda_repo.repository_url}:${var.docker_image_tag}"
   role          = aws_iam_role.lambda_execution_role.arn
 
   environment {
@@ -17,7 +17,7 @@ resource "aws_lambda_function" "health_endpoint" {
 resource "aws_lambda_function" "callback_endpoint" {
   function_name = "callback_endpoint"
   package_type  = "Image"
-  image_uri     = "${aws_ecr_repository.my_lambda_repo.repository_url}:latest"
+  image_uri     = "${aws_ecr_repository.my_lambda_repo.repository_url}:${var.docker_image_tag}"
   role          = aws_iam_role.lambda_execution_role.arn
 
   environment {
@@ -33,7 +33,7 @@ resource "aws_lambda_function" "callback_endpoint" {
 resource "aws_lambda_function" "webhook_endpoint" {
   function_name = "webhook_endpoint"
   package_type  = "Image"
-  image_uri     = "${aws_ecr_repository.my_lambda_repo.repository_url}:latest"
+  image_uri     = "${aws_ecr_repository.my_lambda_repo.repository_url}:${var.docker_image_tag}"
   role          = aws_iam_role.lambda_execution_role.arn
 
   environment {
@@ -51,7 +51,7 @@ resource "aws_lambda_function" "webhook_endpoint" {
 resource "aws_lambda_function" "process_strava_data_trigger" {
   function_name = "process_strava_data_trigger"
   package_type  = "Image"
-  image_uri     = "${aws_ecr_repository.my_lambda_repo.repository_url}:latest"
+  image_uri     = "${aws_ecr_repository.my_lambda_repo.repository_url}:${var.docker_image_tag}"
   role          = aws_iam_role.lambda_execution_role.arn
 
   environment {
@@ -84,7 +84,7 @@ resource "aws_lambda_function_event_invoke_config" "process_strava_data_trigger_
 resource "aws_lambda_function" "prepare_and_upload_gpx" {
   function_name = "prepare_and_upload_gpx"
   package_type  = "Image"
-  image_uri     = "${aws_ecr_repository.my_lambda_repo.repository_url}:latest"
+  image_uri     = "${aws_ecr_repository.my_lambda_repo.repository_url}:${var.docker_image_tag}"
   role          = aws_iam_role.lambda_execution_role.arn
 
   environment {
@@ -101,7 +101,7 @@ resource "aws_lambda_function" "prepare_and_upload_gpx" {
 resource "aws_lambda_function" "prepare_and_upload_parquet" {
   function_name = "prepare_and_upload_parquet"
   package_type  = "Image"
-  image_uri     = "${aws_ecr_repository.my_lambda_repo.repository_url}:latest"
+  image_uri     = "${aws_ecr_repository.my_lambda_repo.repository_url}:${var.docker_image_tag}"
   role          = aws_iam_role.lambda_execution_role.arn
 
   environment {
@@ -119,7 +119,7 @@ resource "aws_lambda_function" "prepare_and_upload_parquet" {
 resource "aws_lambda_function" "store_activity_in_dynamo" {
   function_name = "store_activity_in_dynamo"
   package_type  = "Image"
-  image_uri     = "${aws_ecr_repository.my_lambda_repo.repository_url}:latest"
+  image_uri     = "${aws_ecr_repository.my_lambda_repo.repository_url}:${var.docker_image_tag}"
   role          = aws_iam_role.lambda_execution_role.arn
 
   environment {
@@ -135,7 +135,7 @@ resource "aws_lambda_function" "store_activity_in_dynamo" {
 resource "aws_lambda_function" "check_child_users" {
   function_name = "check_child_users"
   package_type  = "Image"
-  image_uri     = "${aws_ecr_repository.my_lambda_repo.repository_url}:latest"
+  image_uri     = "${aws_ecr_repository.my_lambda_repo.repository_url}:${var.docker_image_tag}"
   role          = aws_iam_role.lambda_execution_role.arn
 
   environment {
@@ -151,7 +151,7 @@ resource "aws_lambda_function" "check_child_users" {
 resource "aws_lambda_function" "validate_child" {
   function_name = "validate_child"
   package_type  = "Image"
-  image_uri     = "${aws_ecr_repository.my_lambda_repo.repository_url}:latest"
+  image_uri     = "${aws_ecr_repository.my_lambda_repo.repository_url}:${var.docker_image_tag}"
   role          = aws_iam_role.lambda_execution_role.arn
 
   environment {
@@ -167,7 +167,7 @@ resource "aws_lambda_function" "validate_child" {
 resource "aws_lambda_function" "duplicate_activity" {
   function_name = "duplicate_activity"
   package_type  = "Image"
-  image_uri     = "${aws_ecr_repository.my_lambda_repo.repository_url}:latest"
+  image_uri     = "${aws_ecr_repository.my_lambda_repo.repository_url}:${var.docker_image_tag}"
   role          = aws_iam_role.lambda_execution_role.arn
 
   environment {
@@ -184,7 +184,7 @@ resource "aws_lambda_function" "duplicate_activity" {
 resource "aws_lambda_function" "check_duplication_status" {
   function_name = "check_duplication_status"
   package_type  = "Image"
-  image_uri     = "${aws_ecr_repository.my_lambda_repo.repository_url}:latest"
+  image_uri     = "${aws_ecr_repository.my_lambda_repo.repository_url}:${var.docker_image_tag}"
   role          = aws_iam_role.lambda_execution_role.arn
 
   environment {
@@ -200,7 +200,7 @@ resource "aws_lambda_function" "check_duplication_status" {
 resource "aws_lambda_function" "delete_activity" {
   function_name = "delete_activity"
   package_type  = "Image"
-  image_uri     = "${aws_ecr_repository.my_lambda_repo.repository_url}:latest"
+  image_uri     = "${aws_ecr_repository.my_lambda_repo.repository_url}:${var.docker_image_tag}"
   role          = aws_iam_role.lambda_execution_role.arn
 
   environment {
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,3 +2,5 @@ variable "aws_region" {
   type    = string
   default = "us-east-1"
 }
+
+variable "docker_image_tag" {}
